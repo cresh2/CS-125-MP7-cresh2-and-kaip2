b9d04a4e920af7b954a287f8d276dd0513e19980
cresh <cresh@DESKTOP-76790BN>
2017-12-02 17:13:54 -0600

    Initial Build

---
+++ CS 125 Choose Your Own Adventure/src/CS125ChooseYourOwnAdventureGame.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author cresh2 and kaip2
+ *
+ */
+public class CS125ChooseYourOwnAdventureGame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
