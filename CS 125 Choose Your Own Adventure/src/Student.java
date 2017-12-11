ea005f8326da3fc8efab4fa6fbb3e425d17a88a5
Kai <Kai@KaiPC>
2017-12-10 21:05:56 -0600

    Student class with create method.

---
+++ CS 125 Choose Your Own Adventure/src/Student.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Kai
+ *
+ */
+public class Student {
+	/**
+	 * Default constructor.
+	 */
+	public Student() {
+		super();
+	}
+	
+	public Scenario create() {
+		return null;
+	}
+}
