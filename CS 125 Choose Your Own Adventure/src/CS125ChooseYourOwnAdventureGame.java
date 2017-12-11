329608a6aa673d9bd12af39ebb04191f33799c69
cresh <cresh@DESKTOP-76790BN>
2017-12-10 22:04:39 -0600

    Hopefully finished the interface.

--- CS 125 Choose Your Own Adventure/src/CS125ChooseYourOwnAdventureGame.java
+++ CS 125 Choose Your Own Adventure/src/CS125ChooseYourOwnAdventureGame.java
@@ -10,25 +10,62 @@ public class CS125ChooseYourOwnAdventureGame {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//Student game = new Student();
+		Student game = new Student();
 		Scanner input = new Scanner(System.in);
 		int answer = 0;
-		//Scenario story = game.create();
+		boolean isValidAnswer = false;
+		Scenario story = game.create();
 		
 		//Beginning of the user interface
 		System.out.println("This is a CS 125 Choose-your-own Adventure! Enjoy!");
 		
 		do {
-			//System.out.println(story.getMessage() + "\n");
-			//if (story.getFirstAnswer != null) {
-				//System.out.println("1. " + story.getFirstAnswer());
-			//}
-			//if (story.getSecondAnswer() != null) {
-				//System.out.println("2. "+ story.getSecondAnswer());
-			//}
-			//if (story.getFirstAnswer == null && story.getSecondAnswer() == null) {
-				//
-			
+			System.out.println(story.getMessage() + "\n");
+			System.out.println("Answers:");
+			if (story.getFirstAnswer() != null) {
+				System.out.println("1. " + story.getFirstAnswer());
+			}
+			if (story.getSecondAnswer() != null) {
+				System.out.println("2. " + story.getSecondAnswer());
+			}
+			if (story.getFirstAnswer() == null && story.getSecondAnswer() == null) {
+				answer = 0;
+			} else {
+				System.out.println("Which answer do you choose? Type 1 for answer 1, 2 for answer 2, or 0 to quit entirely: ");
+				answer = input.nextInt();
+				
+				// Checks if the answer is valid
+				do {
+					switch(answer) {
+						case 0: isValidAnswer = true;
+							break;
+						case 1: if (story.getFirstAnswer() == null) {
+								isValidAnswer = false;
+							} else {
+								isValidAnswer = true;
+							}
+							break;
+						case 2: if (story.getSecondAnswer() == null) {
+								isValidAnswer = false;
+							} else {
+								isValidAnswer = true;
+							}
+							break;
+						default: isValidAnswer = false;
+							break;
+					}
+					if (isValidAnswer == false) {
+						System.out.println("Please enter a valid choice: ");
+						answer = input.nextInt();
+					}
+				} while (isValidAnswer == false);
+			}
+			// Moves on to the corresponding scenario if necessary
+			if (answer == 1) {
+				story = story.getFirstAnswerScenario();
+			} else if (answer == 2) {
+				story = story.getSecondAnswerScenario();
+			}
 		}while (answer != 0);
 		input.close();
 		System.out.print("Thank you for playing!");
